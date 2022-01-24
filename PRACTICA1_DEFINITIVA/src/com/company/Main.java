48946521adf42a26dac8dc5b6f4b0e45ef4303f9
byron hernandez <201806840byron.hernandez@gmail.com>
2022-01-23 23:45:50 -0600

    init
    inicio de la creacion de clases y poroyecto de java

---
+++ PRACTICA1_DEFINITIVA/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("jajajaj ya empezamos la practica ");
+    }
+}
